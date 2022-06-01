3e0554c606093bac7683d0b8a46019ac45108cfe
Aayush Jain <77500156+Aayush-Jain01@users.noreply.github.com>
2022-06-01 16:44:05 +0530

    Changed for weird names of label and image files

--- utils/datasets.py
+++ utils/datasets.py
@@ -370,7 +370,13 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         def img2label_paths(img_paths):
             # Define label paths as a function of image paths
             sa, sb = os.sep + 'images' + os.sep, os.sep + 'labels' + os.sep  # /images/, /labels/ substrings
-            return [x.replace(sa, sb, 1).replace(x.split('.')[-1], 'txt') for x in img_paths]
+#             return [x.replace(sa, sb, 1).replace(x.split('.')[-1], 'txt') for x in img_paths]
+            paths = []
+            for x in img_paths:
+              x = x.replace(sa, sb, 1)
+              x = x[:-4] + '.txt'
+              paths.append(x)
+            return paths
 
         try:
             f = []  # image files
